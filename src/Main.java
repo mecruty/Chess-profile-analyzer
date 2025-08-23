5f32df22bc637c65a2e825f7f63507a624373523
Terry <terrywtr2009@gmail.com>
2025-08-23 14:18:26 -0700

    Refactored main for simplified console output

--- src/Main.java
+++ src/Main.java
@@ -1,32 +1,7 @@
-import java.util.List;
-import java.util.Scanner;
-
-import main.collection.CSVParser;
-import main.collection.GamesCollector;
+import main.console.ConsoleApp;
 
 public class Main {
-    static Scanner sc = new Scanner(System.in);
-
     public static void main(String[] args) {
-        String username;
-        GamesCollector gc;
-        CSVParser csvp;
-
-        System.out.println("Enter username:");
-        username = sc.nextLine();
-        gc = new GamesCollector(username);
-        csvp = new CSVParser(username);
-
-        System.out.println("Would you like to collect data?");
-        if (sc.nextLine().equals("y")) {
-            csvp.toCSV(gc.collectAll());
-            System.out.println("Done");
-        }
-
-        System.out.println("Would you like to load data?");
-        if (sc.nextLine().equals("y")) {
-            List<List<String>> temp = csvp.loadCSV();
-            System.out.println("hi");
-        }
+        new ConsoleApp();
     }
 }
---
+++ src/main/console/ConsoleApp.java
@@ -0,0 +1,37 @@
+package main.console;
+
+import java.util.List;
+import java.util.Scanner;
+
+import main.collection.CSVParser;
+import main.collection.GamesCollector;
+
+public class ConsoleApp {
+    private String username;
+    private GamesCollector gc;
+    private CSVParser csvp;
+    
+    static Scanner sc = new Scanner(System.in);
+
+    public ConsoleApp() {
+        run();
+    }
+
+    private void run() {
+        System.out.println("Enter username:");
+        username = sc.nextLine();
+        gc = new GamesCollector(username);
+        csvp = new CSVParser(username);
+
+        System.out.println("Would you like to collect data?");
+        if (sc.nextLine().equals("y")) {
+            csvp.toCSV(gc.collectAll());
+            System.out.println("Done");
+        }
+
+        System.out.println("Would you like to load data?");
+        if (sc.nextLine().equals("y")) {
+            List<List<String>> csv = csvp.loadCSV();
+        }
+    }
+}
