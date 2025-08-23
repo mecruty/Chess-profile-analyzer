c343ede2186557111ec87e292045956109373973
Terry <terrywtr2009@gmail.com>
2025-08-22 23:03:03 -0700

    Created APIReader class

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+import main.collection.APIReader;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        new APIReader("https://api.chess.com/pub/player/beebeebeeboy/games/archives");
+    }
+}
---
+++ src/main/collection/APIReader.java
@@ -0,0 +1,55 @@
+package main.collection;
+
+import java.net.HttpURLConnection;
+import java.net.URI;
+import java.net.URL;
+import java.util.Scanner;
+
+import org.json.JSONObject;
+
+public class APIReader {
+    URL url;
+
+    public APIReader(String dest) {
+        try {
+            url = new URI(dest).toURL();
+        } catch (Exception e) {
+            throw new RuntimeException("APIReader constructor failed");
+        }
+    }
+
+    public JSONObject read() {
+        try {
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            conn.connect();
+
+            // Getting the response code
+            int responsecode = conn.getResponseCode();
+            // Check if successful
+            if (responsecode != 200) {
+                throw new RuntimeException("HttpResponseCode: " + responsecode);
+            }
+
+            // Collect lines with Scanner
+            String inline = "";
+            Scanner scanner = new Scanner(url.openStream());
+
+            // Write all the JSON data into a string using Scanner
+            while (scanner.hasNext()) {
+                inline += scanner.nextLine();
+            }
+
+            // Close the scanner
+            scanner.close();
+
+            // Converts string to json
+            JSONObject json = new JSONObject(inline);
+
+            return json;
+            
+        } catch (Exception e) {
+            throw new RuntimeException("APIReader read failed");
+        }
+    }
+}
