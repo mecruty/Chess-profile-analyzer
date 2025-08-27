4f79ea61addbeb19506ed51cf559a2f74a5e5c2a
Terry <terrywtr2009@gmail.com>
2025-08-27 00:17:09 -0700

    Finished all simple frequency analyses

--- src/main/analyzer/SimpleFrequencyAnalyzer.java
+++ src/main/analyzer/SimpleFrequencyAnalyzer.java
@@ -12,52 +12,113 @@ public class SimpleFrequencyAnalyzer {
         this.csv = csv;
     }
 
-    public Map<String, List<Integer>> analyzeAll() {
-        Map<String, List<Integer>> analysis = new HashMap<>();
-        
-        analysis.put("rules", analyzeRules());
-        analysis.put("result", analyzeResult());
-        analysis.put("result_detailed", analyzeResultDetailed());
-        analysis.put("eco", analyzeEco());
-        analysis.put("colour", analyzeColour());
-        analysis.put("time_control", analyzeTimeControl());
-        analysis.put("time_class", analyzeTimeClass());
+    public Map<String, Map<String, Integer>> analyzeAll() {
+        Map<String, Map<String, Integer>> analysis = new HashMap<>();
+
+        analysis.put("rulesFrequency", analyzeRules());
+        analysis.put("resultFrequency", analyzeResult());
+        analysis.put("resultDetailedFrequency", analyzeResultDetailed());
+        analysis.put("ecoFrequency", analyzeEco());
+        analysis.put("colourFrequency", analyzeColour());
+        analysis.put("timeControlFrequency", analyzeTimeControl());
+        analysis.put("timeClassFrequency", analyzeTimeClass());
 
         return analysis;
     }
 
-    private List<Integer> analyzeRules() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'analyzeRules'");
+    public Map<String, Integer> analyzeRules() {
+        // Collect values
+        int index = csv.get(0).indexOf("rules");
+        // Has to be index + 1 as 0 is the column names
+        List<String> values = csv.get(index + 1);
+        
+        return countFrequency(values);
     }
 
-    private List<Integer> analyzeResult() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'analyzeResult'");
+    public Map<String, Integer> analyzeResult() {
+        // Collect values
+        int index = csv.get(0).indexOf("result");
+        // Has to be index + 1 as 0 is the column names
+        List<String> values = csv.get(index + 1);
+        
+        return countFrequency(values);
     }
 
-    private List<Integer> analyzeResultDetailed() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'analyzeResultDetailed'");
+    public Map<String, Integer> analyzeResultDetailed() {
+        // Collect values
+        int index = csv.get(0).indexOf("result");
+        // Has to be index + 1 as 0 is the column names
+        List<String> values = csv.get(index + 1);
+
+        // Values for the opponent
+        index = csv.get(0).indexOf("opponent_result");
+        List<String> opponentValues = csv.get(index + 1);
+
+        // Adds the manner in which the opponent lost (opponent_result) if result is win
+        for (int i = 0; i < values.size(); i++) {
+            String value = values.get(i);
+
+            if (value.equals("win")) {
+                value += "(" + opponentValues.get(i) + ")";
+            }
+        }
+        
+        return countFrequency(values);
     }
 
-    private List<Integer> analyzeEco() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'analyzeOpening'");
+    public Map<String, Integer> analyzeEco() {
+        // Collect values
+        int index = csv.get(0).indexOf("eco");
+        // Has to be index + 1 as 0 is the column names
+        List<String> values = csv.get(index + 1);
+
+        // Removes the "https://www.chess.com/openings/" from each opening
+        // Unless there was no opening
+        int indexToRemove = "https://www.chess.com/openings/".length();
+        for (String value : values) {
+            if (!value.equals("none")) {
+                value.substring(indexToRemove);
+            }
+        }
+        
+        return countFrequency(values);
     }
 
-    private List<Integer> analyzeColour() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'analyzeColour'");
+    public Map<String, Integer> analyzeColour() {
+        // Collect values
+        int index = csv.get(0).indexOf("colour");
+        // Has to be index + 1 as 0 is the column names
+        List<String> values = csv.get(index + 1);
+        
+        return countFrequency(values);
     }
 
-    private List<Integer> analyzeTimeControl() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'analyzeTimeControl'");
+    public Map<String, Integer> analyzeTimeControl() {
+        // Collect values
+        int index = csv.get(0).indexOf("time_control");
+        // Has to be index + 1 as 0 is the column names
+        List<String> values = csv.get(index + 1);
+        
+        return countFrequency(values);
     }
 
-    private List<Integer> analyzeTimeClass() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'analyzeTimeClass'");
+    public Map<String, Integer> analyzeTimeClass() {
+        // Collect values
+        int index = csv.get(0).indexOf("time_class");
+        // Has to be index + 1 as 0 is the column names
+        List<String> values = csv.get(index + 1);
+        
+        return countFrequency(values);
     }
+
+    private Map<String, Integer> countFrequency(List<String> values) {
+        Map<String, Integer> analysis = new HashMap<String,Integer>();
+        for (String value : values) {
+            analysis.putIfAbsent(value, 0);
+            analysis.compute(value, (k, v) -> v++);
+        }
+
+        return analysis;
+    }
+    
 }
